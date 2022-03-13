ccc0d4bbff2eb6c0813159769e6e178ccf49f4f1
LawrenceDeacon <lawdeacon.ld@gmail.com>
2022-03-13 13:33:52 +0000

    Created rock paper scissors in console

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,89 @@ let firstName = "lawrence";
 
 console.log(`My Name is ${firstName}`);
 
-(function(a){
+(function (a) {
     console.log(`Display this message ${a}`);
 }("Hello"));
 
 
-let secondName = (x) => {console.log(`${x} is my argument`)};
+let secondName = (x) => { console.log(`${x} is my argument`) };
 secondName("Deacon");
+
+// Rock paper scissors in console
+
+// Computer Choice
+let computerChoice = computerPlays()
+function computerPlays() {
+    let choice = Math.random();
+    // let choice = 0.6;
+    let result;
+    function computation() {
+        if (choice < 0.3) {
+            result = "rock";
+        } else if (choice >= 0.3 && choice <= 0.6) {
+            result = "paper";
+        } else if (choice > 0.6) {
+            result = "scissors"
+        }
+    }
+    computation();
+    return result;
+    // console.log(choice);
+    // console.log(result);
+}
+
+// Player Choice
+function playerChoice (){
+    let choice = prompt().toLowerCase();
+    // console.log(playerChoice);
+    comparison(choice, computerChoice);
+}
+
+function comparison(player, computer) {
+    switch (player) {
+        case "rock":
+            if (computer == "rock") {
+                console.log("This was a tie!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            } else if(computer == "paper"){
+                console.log("You Lose!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            }else{
+                console.log("You Win!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            }
+            break;
+        case "paper":
+            if (computer == "rock") {
+                console.log("You Win!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            } else if(computer == "paper"){
+                console.log("This was a tie!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            }else{
+                console.log("You Lose!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            }
+            break;
+        case "scissors":
+            if (computer == "rock") {
+                console.log("You Lose!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            } else if(computer == "paper"){
+                console.log("You Win!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            }else{
+                console.log("This was a tie!");
+                console.log(`Player chose ${player} & Computer chose ${computer}`)
+            }
+            break;
+
+        default:
+            alert("Try Again!")
+            computerPlays();
+            playerChoice();
+            break;
+    }
+}
+
+playerChoice();
