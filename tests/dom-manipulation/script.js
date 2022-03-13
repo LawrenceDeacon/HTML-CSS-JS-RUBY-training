4c52b64cdd64cbcaf3441fd16f734b66779af29d
LawrenceDeacon <lawdeacon.ld@gmail.com>
2022-03-13 18:31:34 +0000

    More dom manipulation tests

--- tests/dom-manipulation/script.js
+++ tests/dom-manipulation/script.js
@@ -1,32 +1,31 @@
-const wrapper = document.querySelector('.wrapper');
+const div = document.createElement('div');
+div.style.border = "1px solid black";
+div.style.backgroundColor = "pink"
 
-const div1 = document.createElement('div');
+const p = document.createElement('p');
+p.style.color = "red";
+p.textContent = "Hey I'm Red!";
 
-let colour = 'green';
+const h3 = document.createElement('h3');
+h3.style.color = "blue";
+h3.textContent = "Hey I'm a blue H3!";
 
-wrapper.appendChild(div1);
+const h1 = document.createElement('h1');
+h1.textContent = "I'm in a div!"
 
-
-wrapper.textContent = "one two";
-
-
-div1.textContent = 'Hello World';
-
-wrapper.appendChild(div1);
-
-const div2 = document.createElement('div');
-
-div2.textContent = 'Hello World number 2';
-div2.style.border = `1px solid ${colour}`;
-
-wrapper.insertBefore(div2, div1);
+const divP = document.createElement('p');
+divP.textContent = "ME TOO!"
 
 const btn = document.createElement('button');
-btn.textContent = "Click Me";
-btn.style.color = "red";
+btn.textContent = "Click Me!"
+btn.addEventListener("click", (e) => console.log(e));
 
-div1.appendChild(btn);
 
-btn.addEventListener('click', function(){
-    alert('You clicked the button!');
-});
+div.appendChild(h1);
+div.appendChild(divP);
+div.appendChild(btn);
+
+document.body.appendChild(div);
+document.body.appendChild(p);
+document.body.appendChild(h3);
+
