dd744b67190ee27b6322ed33936c8b256bbcd82e
LawrenceDeacon <lawdeacon.ld@gmail.com>
2022-03-16 10:29:09 +0000

    Returns substring but does not count

--- tests/sub_strings/ruby.rb
+++ tests/sub_strings/ruby.rb
@@ -2,6 +2,8 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(string, dictionary)
     results = []
+    resultsCount = {}
+
     # p string
     # p dictionary
     
@@ -9,11 +11,18 @@ def substrings(string, dictionary)
         if string.include?(x)           
             results.push(x)
         end
+        p results
     }
 
-    resultsCount = {}
+    results.each{|x|
+        p x
+        resultsCount.store(x, resultsCount[x] != nil ? resultsCount[x] + 1 : resultsCount[x] = 1) 
+    }
 
-    p results
+    p resultsCount
 end
 
-substrings("belowlow", dictionary)
+puts("Enter your string: ")
+string = gets
+
+substrings(string, dictionary)
